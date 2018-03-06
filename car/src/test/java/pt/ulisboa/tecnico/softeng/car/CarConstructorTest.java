16c2c249466b0811f332cb353d6b282a12725f56
Pedro Caldeira <caldeira.a.pedro@gmail.com>
2018-03-06 16:24:24 +0000

    closes #41

--- car/src/test/java/pt/ulisboa/tecnico/softeng/car/CarConstructorTest.java
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/CarConstructorTest.java
@@ -28,6 +28,23 @@ public class CarConstructorTest {
 		Assert.assertFalse(car.getRentACar() == "");
 		Assert.assertNotNull(car.getRentACar());
 		
+		//Plates cant be null and need to have the format: "XX-XX-XX"
+		Assert.assertNotNull(car.getPlate());
+		
+		Assert.assertNotNull(car.getPlate()[0]);
+		Assert.assertNotNull(car.getPlate()[1]);
+		Assert.assertNotNull(car.getPlate()[3]);
+		Assert.assertNotNull(car.getPlate()[4]);
+		Assert.assertNotNull(car.getPlate()[6]);
+		Assert.assertNotNull(car.getPlate()[7]);
+	
+		Assert.assertEquals("-", car.getPlate()[2]);
+		Assert.assertEquals("-", car.getPlate()[5]);
+		
+		//Plates cant be duplicate
+		for(Car c : _vehicles) {
+			Assert.assertNotSame(car.getPlate(), c.getPlate());
+		}
 	}
 	
 	@After
