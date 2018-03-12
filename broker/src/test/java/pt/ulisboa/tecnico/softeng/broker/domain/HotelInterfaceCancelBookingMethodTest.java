4c73fae3801d88de2418a5ff335e9d096bdd4574
Mariana <marianagprmendes@gmail.com>
2018-03-12 01:53:22 +0000

    closes #101 Criar testes para o método cancelBooking() commit

---
+++ broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/HotelInterfaceCancelBookingMethodTest.java
@@ -0,0 +1,51 @@
+package pt.ulisboa.tecnico.softeng.broker.domain;
+
+import org.joda.time.LocalDate;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.ulisboa.tecnico.softeng.broker.exception.BrokerException;
+import pt.ulisboa.tecnico.softeng.broker.domain.BulkRoomBooking;
+import pt.ulisboa.tecnico.softeng.hotel.domain.Hotel;
+import pt.ulisboa.tecnico.softeng.hotel.domain.Booking;
+import pt.ulisboa.tecnico.softeng.hotel.domain.Room;
+import pt.ulisboa.tecnico.softeng.hotel.domain.Room.Type;
+import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.HotelInterface;
+
+public class HotelInterfaceCancelBookingMethodTest {
+	private final int number = 1;	
+	private final LocalDate arrival = new LocalDate(2016, 12, 19);
+	private final LocalDate departure = new LocalDate(2016, 12, 21);
+	private Hotel hotel;
+	private Room room;
+	private Booking booking;
+	
+	@Before
+	public void setUp() {
+		this.hotel = new Hotel("XPTO123", "Paris");
+		this.room = new Room(this.hotel, "01", Type.DOUBLE);
+		this.booking = this.room.reserve(Type.DOUBLE, this.arrival, this.departure);
+	}
+	
+	@Test
+	public void success() {
+		Assert.assertNotNull(HotelInterface.cancelBooking(this.booking.getReference()));
+	}
+	
+	@Test(expected = HotelException.class)
+	public void exceptionTest() {
+		HotelInterface hotelinterface = new HotelInterface();
+		hotelinterface.cancelBooking("XPTO");
+		
+	}
+	
+	@After
+	public void tearDown() {
+		Hotel.hotels.clear();
+	}
+	
+
+}
