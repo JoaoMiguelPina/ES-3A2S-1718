8c9aab5bdd44e3985be21aa87e606ed1c150649f
Mariana <marianagprmendes@gmail.com>
2018-03-12 20:04:54 +0000

    closes #124 Criar e implementar MotorcycleException commit.

---
+++ car/src/main/java/pt/ulisboa/tecnico/softeng/car/exception/MotorcycleException.java
@@ -0,0 +1,11 @@
+package pt.ulisboa.tecnico.softeng.car.exception;
+
+public class MotorcycleException extends RuntimeException {
+  public MotorcycleException() {
+    super();
+  }
+
+  public MotorcycleException(String message) {
+    super(message);
+  }
+}
