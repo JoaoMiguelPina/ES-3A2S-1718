58c6e5bc5a8a9f78731e13d8f264806fb993c314
Mariana <marianagprmendes@gmail.com>
2018-03-12 16:03:11 +0000

    closes #112 Testes para o construtor Car.

--- car/src/test/java/pt/ulisboa/tecnico/softeng/car/CarConstructorTest.java
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/CarConstructorTest.java
@@ -6,52 +6,25 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import pt.ulisboa.tecnico.softeng.car.Car;
+import pt.ulisboa.tecnico.softeng.car.RentACar;
 
 public class CarConstructorTest {
+	private Car car;
+	private RentACar rentACar;
 
 	@Before
 	public void setUp() {
-		Car car = new Car("12-14-CJ", 34, "Car Deluxe");
+		this.car = new Car("12-14-CJ", 34, "Car Deluxe");
+		this.rentACar = new RentACar();
 	}
 
 	@Test
 	public void success() {
-
-		Assert.assertEquals("12-14-CJ", car.getPlate());
-		Assert.assertEquals(34, car.getKm());
-		Assert.assertEquals("Car Deluxe", car.getRentACar());
+		junit.framework.Assert.assertEquals("12-14-CJ", this.car.getPlate());
+		junit.framework.Assert.assertEquals(34, this.car.getKilometers());
+		junit.framework.Assert.assertNotNull(this.car.getRentACar());
 		
-		//Car cant have negative kms
-		Assert.assertTrue(car.getKilometers() >= 0);
-		
-		//RentACar cant have a null nor empty name
-		Assert.assertFalse(car.getRentACar() == "");
-		Assert.assertNotNull(car.getRentACar());
-		
-		//Plates cant be null and need to have the format: "XX-XX-XX"
-		Assert.assertNotNull(car.getPlate());
-		
-		Assert.assertNotNull(car.getPlate()[0]);
-		Assert.assertNotNull(car.getPlate()[1]);
-		Assert.assertNotNull(car.getPlate()[3]);
-		Assert.assertNotNull(car.getPlate()[4]);
-		Assert.assertNotNull(car.getPlate()[6]);
-		Assert.assertNotNull(car.getPlate()[7]);
-	
-		Assert.assertEquals("-", car.getPlate()[2]);
-		Assert.assertEquals("-", car.getPlate()[5]);
-		
-		//Plates cant be duplicate
-		for(Car c : _vehicles) {
-			Assert.assertNotSame(car.getPlate(), c.getPlate());
-		}
-		
-		
-	}
-	
-	@After
-	public void tearDown() {
-		car.destroyCar();
 	}
 
 }
