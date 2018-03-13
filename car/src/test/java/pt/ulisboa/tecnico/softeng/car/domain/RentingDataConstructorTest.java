809be731576cbfafea348258b5063c51fc4a8950
Pedro Caldeira <caldeira.a.pedro@gmail.com>
2018-03-13 15:30:41 +0000

    closes #117

--- car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentingDataConstructorTest.java
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentingDataConstructorTest.java
@@ -13,6 +13,8 @@ public class RentingDataConstructorTest {
 	public void setUp() {
 		Car car = new Car("12-14-CJ", 34, "Car Deluxe");
 		Motorcycle motor = new Motorcycle("66-65-OC", 80, "Top Fleet");
+		RentingData rd = new RentingData("1234", "66-67-TC", "AAA111", "Top Fleet", java.time.LocalDate.now(), java.time.LocalDate.now());
+	
 	}
 
 	@Test
@@ -23,11 +25,20 @@ public class RentingDataConstructorTest {
 		Assert.assertNotNull(car.getRentACar());
 		Assert.assertNotNull(motor.getRentACar());
 		
+		//RentingData Constructor test
+		Assert.assertEquals("1234", rd.getReference());
+		Assert.assertArrayEquals("66-67-TC", rd.getPlate());
+		Assert.assertArrayEquals("AAA111", rd.getLicense());
+		Assert.assertArrayEquals("Top Fleet", rd.getRentACar());
+		Assert.assertArrayEquals(java.time.LocalDate.now(), rd.getBegin());
+		Assert.assertArrayEquals(java.time.LocalDate.now(), rd.getEnd());
+		
 	}
 	
 	@After
 	public void tearDown() {
 		car.destroyCar();
+		motor.destroyMotorcycle();
 	}
 
 }
