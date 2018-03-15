22f90923dbb7cce5ea17a3cd8c7003f2b136b33c
pedrodaniel10 <pedro.daniel.l@tecnico.ulisboa.pt>
2018-03-15 19:13:13 +0000

    closes #159

--- tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/dataobjects/InvoiceData.java
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/dataobjects/InvoiceData.java
@@ -12,9 +12,7 @@ public class InvoiceData {
 	private LocalDate date;
 	
 	public InvoiceData(String sellerNIF, String buyerNIF, String itemType, float value, LocalDate date) {
-		if(date.getYear() < 1970){
-			throw new TaxException();
-		}
+		checkArguments(sellerNIF, buyerNIF, itemType, value, date);
 		
 		this.sellerNIF = sellerNIF;
 		this.buyerNIF = buyerNIF;
@@ -22,7 +20,48 @@ public class InvoiceData {
 		this.value = value;
 		this.date = date;
 	}
+	
+	private void checkArguments(String sellerNIF, String buyerNIF, String itemType, float value, LocalDate date) {
+		checkNIF(sellerNIF);
+		checkNIF(buyerNIF);
+		checkItemType(itemType);
+		checkValue(value);
+		checkDate(date);
+	}
+	
+	private void checkNIF(String nif){
+		if(nif == null || nif.equals("") || (nif.trim()).equals("")){
+			throw new TaxException();
+		}
+		if(nif.length() != 9){
+			throw new TaxException();
+		}
+		if(!nif.matches("[0-9]+")){
+			throw new TaxException();
+		}
+	}
 
+	private void checkItemType(String itemType){
+		if(itemType == null || itemType.equals("") || (itemType.trim()).equals("")){
+			throw new TaxException();
+		}
+	}
+	
+	private void checkValue(float value){
+		if(value < 0){
+			throw new TaxException();
+		}
+	}
+	
+	private void checkDate(LocalDate date){
+		if(date == null){
+			throw new TaxException();
+		}
+		if(date.getYear() < 1970){
+			throw new TaxException();
+		}
+	}
+	
 	public String getSellerNIF() {
 		return sellerNIF;
 	}
