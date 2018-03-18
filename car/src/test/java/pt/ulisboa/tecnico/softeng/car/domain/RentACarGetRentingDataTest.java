6109f46940e9d83bde77acf567ac38a0f5cbae2c
Pedro Caldeira <caldeira.a.pedro@gmail.com>
2018-03-18 14:33:48 +0000

    menos uns warnings

--- car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetRentingDataTest.java
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetRentingDataTest.java
@@ -3,7 +3,6 @@ package pt.ulisboa.tecnico.softeng.car.domain;
 import static org.junit.Assert.*;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
