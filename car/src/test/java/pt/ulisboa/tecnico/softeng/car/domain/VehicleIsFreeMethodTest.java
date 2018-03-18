5b47d93881b47b23ba09d6cd11aa7bfc0ad1b657
Mariana <marianagprmendes@gmail.com>
2018-03-18 13:58:35 +0000

    closes #195

---
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/VehicleIsFreeMethodTest.java
@@ -0,0 +1,49 @@
+package pt.ulisboa.tecnico.softeng.car.domain;
+
+import org.joda.time.LocalDate;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.ulisboa.tecnico.softeng.car.exception.CarException;
+
+
+public class VehicleIsFreeMethodTest {
+
+	private final String drivingLicence = "IMTT000";
+	private final LocalDate arrival = new LocalDate(2016, 12, 19);
+	private final LocalDate departure = new LocalDate(2016, 12, 24);
+	private Car car;
+	private Renting renting;
+	
+
+	@Before
+	public void setUp() {
+		RentACar rentACar = new RentACar("New rent");
+		this.car = new Car("14-14-GG", 0, rentACar);
+		this.renting = this.car.rent(this.drivingLicence, this.arrival, this.departure);
+	}
+
+	@Test
+	public void success() {
+		Assert.assertTrue(this.car.isFree(this.arrival.minusDays(10), this.arrival.minusDays(4)));
+	}
+	
+	@Test
+	public void notFree() {
+		Assert.assertFalse(this.car.isFree(this.arrival, this.departure));
+	}
+
+	@Test (expected = CarException.class)
+	public void invalidDates() {
+		Assert.assertTrue(this.car.isFree(this.departure, this.arrival));
+	}
+	
+	@After
+	public void tearDown() {
+		Vehicle.vehicles.clear();
+		RentACar.rentACars.clear();
+	}
+
+}
