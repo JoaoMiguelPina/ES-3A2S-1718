a5ea4ae448e57fcf921f80662c21ad0a532b9fe6
Pedro Caldeira <caldeira.a.pedro@gmail.com>
2018-04-07 22:59:06 +0100

    ProcessorSubmitRentingMethodTest quase terminado

---
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/ProcessorSubmitRentingMethodTest.java
@@ -0,0 +1,308 @@
+package pt.ulisboa.tecnico.softeng.car.domain;
+
+import org.joda.time.LocalDate;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import mockit.Delegate;
+import mockit.Expectations;
+import mockit.FullVerifications;
+import mockit.Mocked;
+import mockit.integration.junit4.JMockit;
+import pt.ulisboa.tecnico.softeng.car.exception.RemoteAccessException;
+import pt.ulisboa.tecnico.softeng.car.interfaces.BankInterface;
+import pt.ulisboa.tecnico.softeng.car.interfaces.TaxInterface;
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
+import pt.ulisboa.tecnico.softeng.tax.dataobjects.InvoiceData;
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
+
+@RunWith(JMockit.class)
+public class ProcessorSubmitRentingMethodTest {
+	private static final String CANCEL_PAYMENT_REFERENCE = "CancelPaymentReference";
+	private static final String INVOICE_REFERENCE = "InvoiceReference";
+	private static final String PAYMENT_REFERENCE = "PaymentReference";
+	public String CAR_PLATE;
+	public String DRIVERS_LICENSE;
+	public LocalDate begin;
+	public LocalDate end;
+	private Renting renting;
+	private RentACar rentACar;
+	private Car car;
+
+	@Before
+	public void setUp() {
+		
+		this.DRIVERS_LICENSE = "abc123456789";
+		this.CAR_PLATE = "FF-90-09";
+		LocalDate begin = new LocalDate(2016, 12, 19);
+		LocalDate end = new LocalDate(2016, 12, 21);
+		this.renting = new Renting(DRIVERS_LICENSE, begin, end, car);
+	}
+
+	@Test
+	public void success(@Mocked final TaxInterface taxInterface, @Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+			}
+		};
+
+		this.rentACar.getProcessor().submitRenting(this.renting);
+
+		new FullVerifications() {
+			{
+			}
+		};
+	}
+
+	@Test
+	public void oneTaxFailureOnSubmitInvoice(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.result = PAYMENT_REFERENCE;
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.result = new TaxException();
+				this.result = INVOICE_REFERENCE;
+			}
+		};
+
+		this.rentACar.getProcessor().submitRenting(this.renting);
+		this.rentACar.getProcessor().submitRenting(new Renting(DRIVERS_LICENSE, begin, end, car));
+
+		new FullVerifications(taxInterface) {
+			{
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.times = 3;
+			}
+		};
+	}
+
+	@Test
+	public void oneRemoteFailureOnSubmitInvoice(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.result = PAYMENT_REFERENCE;
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.result = new RemoteAccessException();
+				this.result = INVOICE_REFERENCE;
+			}
+		};
+
+		this.rentACar.getProcessor().submitRenting(this.renting);
+		this.rentACar.getProcessor().submitRenting(new Renting(DRIVERS_LICENSE, begin, end, car));
+
+		new FullVerifications(taxInterface) {
+			{
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.times = 3;
+			}
+		};
+	}
+
+	@Test
+	public void oneBankFailureOnProcessPayment(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.result = new BankException();
+				this.result = PAYMENT_REFERENCE;
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.result = INVOICE_REFERENCE;
+			}
+		};
+
+		this.rentACar.getProcessor().submitRenting(this.renting);
+		this.rentACar.getProcessor().submitRenting(new Renting(DRIVERS_LICENSE, begin, end, car));
+
+		new FullVerifications(bankInterface) {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.times = 3;
+			}
+		};
+	}
+
+	@Test
+	public void oneRemoteFailureOnProcessPayment(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.result = new RemoteAccessException();
+				this.result = PAYMENT_REFERENCE;
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.result = INVOICE_REFERENCE;
+			}
+		};
+
+		this.rentACar.getProcessor().submitRenting(this.renting);
+		this.rentACar.getProcessor().submitRenting(new Renting(DRIVERS_LICENSE, begin, end, car));
+
+		new FullVerifications(bankInterface) {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.times = 3;
+			}
+		};
+	}
+
+	@Test
+	public void successCancel(@Mocked final TaxInterface taxInterface, @Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+
+				TaxInterface.cancelInvoice(this.anyString);
+				BankInterface.cancelPayment(this.anyString);
+			}
+		};
+
+		this.rentACar.getProcessor().submitRenting(this.renting);
+		this.renting.cancel();
+
+		new FullVerifications() {
+			{
+			}
+		};
+	}
+
+	@Test
+	public void oneBankExceptionOnCancelPayment(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+
+				BankInterface.cancelPayment(this.anyString);
+				this.result = new BankException();
+				this.result = CANCEL_PAYMENT_REFERENCE;
+				TaxInterface.cancelInvoice(this.anyString);
+			}
+		};
+
+		this.rentACar.getProcessor().submitRenting(this.renting);
+		this.renting.cancel();
+		this.rentACar.getProcessor().submitRenting(new Renting(DRIVERS_LICENSE, begin, end, car));
+
+		new FullVerifications(bankInterface) {
+			{
+				BankInterface.cancelPayment(this.anyString);
+				this.times = 2;
+			}
+		};
+	}
+
+	@Test
+	public void oneRemoteExceptionOnCancelPayment(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+
+				BankInterface.cancelPayment(this.anyString);
+				this.result = new RemoteAccessException();
+				this.result = CANCEL_PAYMENT_REFERENCE;
+				TaxInterface.cancelInvoice(this.anyString);
+			}
+		};
+
+		this.rentACar.getProcessor().submitRenting(this.renting);
+		this.renting.cancel();
+		this.rentACar.getProcessor().submitRenting(new Renting(DRIVERS_LICENSE, begin, end, car));
+
+		new FullVerifications(bankInterface) {
+			{
+				BankInterface.cancelPayment(this.anyString);
+				this.times = 2;
+			}
+		};
+	}
+
+	@Test
+	public void oneTaxExceptionOnCancelInvoice(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				BankInterface.cancelPayment(this.anyString);
+				this.result = CANCEL_PAYMENT_REFERENCE;
+				TaxInterface.cancelInvoice(this.anyString);
+				this.result = new Delegate() {
+					int i = 0;
+
+					public void delegate() {
+						if (this.i < 1) {
+							this.i++;
+							throw new TaxException();
+						}
+					}
+				};
+			}
+		};
+
+		this.rentACar.getProcessor().submitRenting(this.renting);
+		this.renting.cancel();
+		this.rentACar.getProcessor().submitRenting(new Renting(DRIVERS_LICENSE, begin, end, car));
+
+		new FullVerifications(taxInterface) {
+			{
+				TaxInterface.cancelInvoice(this.anyString);
+				this.times = 2;
+			}
+		};
+	}
+
+	@Test
+	public void oneRemoteExceptionOnCancelInvoice(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+
+				BankInterface.cancelPayment(this.anyString);
+				this.result = CANCEL_PAYMENT_REFERENCE;
+				TaxInterface.cancelInvoice(this.anyString);
+				this.result = new Delegate() {
+					int i = 0;
+
+					public void delegate() {
+						if (this.i < 1) {
+							this.i++;
+							throw new RemoteAccessException();
+						}
+					}
+				};
+			}
+		};
+
+		this.rentACar.getProcessor().submitRenting(this.renting);
+		this.renting.cancel();
+		this.rentACar.getProcessor().submitRenting(new Renting(DRIVERS_LICENSE, begin, end, car));
+
+		new FullVerifications(taxInterface) {
+			{
+				TaxInterface.cancelInvoice(this.anyString);
+				this.times = 2;
+			}
+		};
+	}
+
+	@After
+	public void tearDown() {
+		RentACar.rentACars.clear();
+	}
+
+}
