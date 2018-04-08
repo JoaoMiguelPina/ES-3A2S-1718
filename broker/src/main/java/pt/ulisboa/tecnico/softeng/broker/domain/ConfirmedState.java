4ceee9adea33433ae665efffbbb22f2a09259b70
regouga <miguelregouga@tecnico.ulisboa.pt>
2018-04-08 11:18:30 +0100

    Closes #230

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/ConfirmedState.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/ConfirmedState.java
@@ -1,16 +1,21 @@
 package pt.ulisboa.tecnico.softeng.broker.domain;
 
+import java.rmi.RemoteException;
+
 import pt.ulisboa.tecnico.softeng.activity.exception.ActivityException;
 import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
 import pt.ulisboa.tecnico.softeng.broker.domain.Adventure.State;
 import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
 import pt.ulisboa.tecnico.softeng.broker.interfaces.ActivityInterface;
 import pt.ulisboa.tecnico.softeng.broker.interfaces.BankInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.CarInterface;
 import pt.ulisboa.tecnico.softeng.broker.interfaces.HotelInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.TaxInterface;
+import pt.ulisboa.tecnico.softeng.car.exception.CarException;
 import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
 
 public class ConfirmedState extends AdventureState {
-	public static int MAX_REMOTE_ERRORS = 20;
 	public static int MAX_BANK_EXCEPTIONS = 5;
 
 	private int numberOfBankExceptions = 0;
@@ -31,13 +36,10 @@ public class ConfirmedState extends AdventureState {
 			}
 			return;
 		} catch (RemoteAccessException rae) {
-			incNumOfRemoteErrors();
-			if (getNumOfRemoteErrors() == MAX_REMOTE_ERRORS) {
-				adventure.setState(State.UNDO);
-			}
+			adventure.setState(State.CONFIRMED);
 			return;
 		}
-		resetNumOfRemoteErrors();
+		
 		this.numberOfBankExceptions = 0;
 
 		try {
@@ -46,13 +48,9 @@ public class ConfirmedState extends AdventureState {
 			adventure.setState(State.UNDO);
 			return;
 		} catch (RemoteAccessException rae) {
-			incNumOfRemoteErrors();
-			if (getNumOfRemoteErrors() == MAX_REMOTE_ERRORS) {
-				adventure.setState(State.UNDO);
-			}
+			adventure.setState(State.CONFIRMED);
 			return;
 		}
-		resetNumOfRemoteErrors();
 
 		if (adventure.getRoomConfirmation() != null) {
 			try {
@@ -61,13 +59,33 @@ public class ConfirmedState extends AdventureState {
 				adventure.setState(State.UNDO);
 				return;
 			} catch (RemoteAccessException rae) {
-				incNumOfRemoteErrors();
-				if (getNumOfRemoteErrors() == MAX_REMOTE_ERRORS) {
-					adventure.setState(State.UNDO);
-				}
+				adventure.setState(State.CONFIRMED);
 				return;
 			}
-			resetNumOfRemoteErrors();
+		}
+		
+		if (adventure.getVehicleConfirmation() != null) {
+			try {
+				CarInterface.getRentingData(adventure.getVehicleConfirmation());
+			} catch (CarException ce) {
+				adventure.setState(State.UNDO);
+				return;
+			} catch (RemoteAccessException rae) {
+				adventure.setState(State.CONFIRMED);
+				return;
+			}
+		}
+		
+		if (adventure.getInvoiceConfirmation() != null) {
+			try {
+				TaxInterface.getInvoiceData(adventure.getInvoiceConfirmation());
+			} catch (TaxException e) {
+				adventure.setState(State.UNDO);
+				return;
+			} catch (RemoteAccessException rae) {
+				adventure.setState(State.CONFIRMED);
+				return;
+			}
 		}
 
 		// TODO: prints the complete Adventure file, the info in operation,
